9b17bb028c8dfdc8e76805434ba62d5e53c99dc2
Daniel Gallichan <dgallichan@yahoo.com>
2023-03-09 13:23:15 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,8 +10,12 @@ input.onButtonPressed(Button.B, function () {
         kitronik_i2c_16_servo.adjustServos(adjust)
     }
 })
+let thisAngle = 0
 let adjust = 0
-adjust = -16
+// -16 is the correct value for first Tower Pro SG92R servo in position 1
+// -13 is correct for a 9g SG90
+adjust = -20
+kitronik_i2c_16_servo.adjustServos(adjust)
 basic.showLeds(`
     . . # . .
     . . . . .
@@ -24,10 +28,34 @@ basic.forever(function () {
     basic.pause(100)
 })
 basic.forever(function () {
-    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0)
+    thisAngle = 0
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, thisAngle)
     basic.pause(1000)
-    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 90)
+    thisAngle = 90
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, thisAngle)
     basic.pause(1000)
-    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 180)
-    basic.pause(1000)
+    thisAngle = 180
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo5, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo7, thisAngle)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, thisAngle)
+    basic.pause(2000)
 })
